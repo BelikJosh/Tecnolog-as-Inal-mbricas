ca4891b889c3c8ba04df6d282cf274900a15c979
BelikJosh <152908088+BelikJosh@users.noreply.github.com>
2024-10-12 18:56:29 -0600

    Add files via upload

---
+++ Tecnlogias/Tecnlogias/Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿
+namespace Tecnlogias
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.capastxt = new System.Windows.Forms.TextBox();
+            this.Calcular = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // capastxt
+            // 
+            this.capastxt.Location = new System.Drawing.Point(106, 136);
+            this.capastxt.Name = "capastxt";
+            this.capastxt.Size = new System.Drawing.Size(263, 22);
+            this.capastxt.TabIndex = 1;
+            // 
+            // Calcular
+            // 
+            this.Calcular.BackColor = System.Drawing.Color.DarkSalmon;
+            this.Calcular.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Calcular.Location = new System.Drawing.Point(185, 235);
+            this.Calcular.Name = "Calcular";
+            this.Calcular.Size = new System.Drawing.Size(100, 55);
+            this.Calcular.TabIndex = 2;
+            this.Calcular.Text = "Calcular";
+            this.Calcular.UseVisualStyleBackColor = false;
+            this.Calcular.Click += new System.EventHandler(this.Calcular_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 25.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(49, 30);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(402, 52);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Cantidad de capas";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(463, 448);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Calcular);
+            this.Controls.Add(this.capastxt);
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox capastxt;
+        private System.Windows.Forms.Button Calcular;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Tecnlogias/Tecnlogias/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tecnlogias
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        double potencia = 55;
+        int porcentaje = 0;
+        int pt = 100;
+
+        private void Calcular_Click(object sender, EventArgs e)
+        {
+            // Verificamos si el campo de texto tiene un valor válido
+            if (int.TryParse(capastxt.Text, out int cantidad))
+            {
+                StringBuilder resultadoPotencias = new StringBuilder();
+                resultadoPotencias.AppendLine("Resultados de potencia por cada capa:");
+
+                // Reiniciamos las variables para calcular correctamente en cada intento
+                potencia = 55;
+                porcentaje = 0;
+                pt = 100;
+
+                for (int i = 0; i < cantidad; i++)
+                {
+                    porcentaje = porcentaje + 10;
+                    pt = pt - porcentaje;
+                    potencia = (pt * potencia) / 100;
+
+                    // Agregamos el resultado de la potencia en esta capa
+                    resultadoPotencias.AppendLine($"Capa {i + 1}: Potencia = {potencia}");
+                }
+
+                // Mostramos todas las potencias resultantes por capa
+                MessageBox.Show(resultadoPotencias.ToString(), "Resultados de Potencia");
+                this.Close();
+            }
+            else
+            {
+                // Mostrar mensaje si el valor ingresado no es válido
+                MessageBox.Show("Por favor, ingrese un valor numérico válido en la cantidad de capas.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
